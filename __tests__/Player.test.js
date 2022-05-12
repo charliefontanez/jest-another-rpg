ace3004e652a4482c6230ac056adbaed6379b3de
Carlos Fontanez <charliefontanezis@gmail.com>
2022-05-12 15:34:13 -0400

    prototype test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,9 @@ const Player = require('../lib/Player');
 
 const Potion = require('../lib/Potion');
 
-jest.mock('../lib/Potion');
+// jest.mock('../lib/Potion');
+
+console.log(new Potion());
 
 test('creates a player object', () => {
   const player = new Player('Dave');
@@ -18,4 +20,21 @@ test('creates a player object', () => {
 });
 
 
-console.log(new Potion());
+// test("gets player's stats as an object", () => {
+//   const player = new Player('Dave');
+
+//   expect(player.getStats()).toHaveProperty('potions');
+//   expect(player.getStats()).toHaveProperty('health');
+//   expect(player.getStats()).toHaveProperty('strength');
+//   expect(player.getStats()).toHaveProperty('agility');
+// });
+
+// test('gets inventory from player or returns false', () => {
+//   const player = new Player('Dave');
+
+//   expect(player.getInventory()).toEqual(expect.any(Array));
+
+//   player.inventory = [];
+
+//   expect(player.getInventory()).toEqual(false);
+// });
